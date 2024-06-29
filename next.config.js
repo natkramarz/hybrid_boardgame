688bbf437bc78b23c0c17c7a674e325ce2e7c785
Natalia Kramarz <nat.kramarz@gmail.com>
2024-06-29 22:29:36 +0200

    Chuj

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  
+  reactStrictMode: true,
+  basePath: "/hybrid_boardgame"
+};
+
+
+export default nextConfig;
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",  
-    reactStrictMode: true,
-    basePath: "/hybrid_boardgame"
-  };
-  
-module.exports = nextConfig;
-//export default nextConfig;
